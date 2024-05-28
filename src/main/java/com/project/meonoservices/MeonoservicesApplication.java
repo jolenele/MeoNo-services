840efe98052f13a59361eae5927e060a491a4f9e
Jolene Le <38019858+jolenele@users.noreply.github.com>
2024-05-27 23:38:49 -0400

    Initial commit

---
+++ src/main/java/com/project/meonoservices/MeonoservicesApplication.java
@@ -0,0 +1,13 @@
+package com.project.meonoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeonoservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeonoservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/meonoservices/MeonoservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.meonoservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeonoservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
